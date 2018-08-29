84e855c7d757ae113b36caf76d28ad406b0253d4
Nathan Miranda <nate845@gmail.com>
2018-08-29 01:15:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function fetchBooks() {
   //write fetch request to the Game of Thrones API
+  fetch('https://anapioficeandfire.com/api/books')
+  .then((resp) => { return resp.json()})
+  .then(json => renderBooks(json));
 }
 
 function renderBooks(json) {
